b3f5d05c9de216eea0b3c8edb28b0696ae9ebca2
Mohammed Rezq <mohammedrezq2000@gmail.com>
2021-09-12 18:46:21 +0200

    [LOZ-10] Remove Avatar and Author, Add Some Styles for Post Preview

--- components/post-preview.js
+++ components/post-preview.js
@@ -3,6 +3,8 @@ import Date from '../components/date'
 import CoverImage from './cover-image'
 import Link from 'next/link'
 
+import styles from './post-preview.module.scss';
+
 export default function PostPreview({
   title,
   coverImage,
@@ -12,12 +14,13 @@ export default function PostPreview({
   slug,
 }) {
   return (
-    <div className="border-2">
+    <div className={styles.postPreview}>
       <div className="mb-5">
         {coverImage && (
           <CoverImage title={title} coverImage={coverImage} slug={slug} />
         )}
       </div>
+      <div className={styles.postPreviewContent}>
       <h3 className="text-3xl mb-3 leading-snug">
         <Link href={`/posts/${slug}`}>
           <a
@@ -33,7 +36,8 @@ export default function PostPreview({
         className="text-lg leading-relaxed mb-4"
         dangerouslySetInnerHTML={{ __html: excerpt }}
       />
-      <Avatar author={author} />
+      {/* <Avatar author={author} /> */}
+      </div>
     </div>
   )
 }
